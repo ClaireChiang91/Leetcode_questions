bdc9be40e18ca5e4e1437f29037b623eb1a08275
Claire Chiang <clairechiang91@gmail.com>
2021-10-22 23:30:56 -0400

    Update 346_Moving Average_from_Data_Stream.py

--- Algorithms/346_Moving Average_from_Data_Stream.py
+++ Algorithms/346_Moving Average_from_Data_Stream.py
@@ -1,3 +1,23 @@
+"""
+The below test case provided by Leetcode:
+
+Input
+["MovingAverage", "next", "next", "next", "next"]
+[[3], [1], [10], [3], [5]]
+
+=>[3] as the size
+=>[1], [10], [3], [5] are the stream of integers 
+
+Output
+[null, 1.0, 5.5, 4.66667, 6.0]
+
+Explanation
+MovingAverage movingAverage = new MovingAverage(3);
+movingAverage.next(1); // return 1.0 = 1 / 1
+movingAverage.next(10); // return 5.5 = (1 + 10) / 2
+movingAverage.next(3); // return 4.66667 = (1 + 10 + 3) / 3
+movingAverage.next(5); // return 6.0 = (10 + 3 + 5) / 3
+"""
 class MovingAverage:
 
     def __init__(self, size: int):
