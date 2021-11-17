dd8b502108069bdf0dd6b32a9061a895da27c913
Claire Chiang <clairechiang91@gmail.com>
2021-11-16 22:27:46 -0500

    Update 578_Get_Highest_Answer_Rate_Question.sql

--- Database_Qs/578_Get_Highest_Answer_Rate_Question.sql
+++ Database_Qs/578_Get_Highest_Answer_Rate_Question.sql
@@ -1,11 +1,12 @@
 #the question didn't ask us to output the rate
 #the only request is to ask give the question with the highest answer rate 
-select question_id as survey_log
-from surveylog
-where answer_id  is not null
-group by question_id
-order by count(*) desc
-limit 1
+
+SELECT question_id AS survey_log FROM SurveyLog
+WHERE answer_id IS NOT NULL
+GROUP BY question_id
+ORDER BY count(*) DESC 
+LIMIT 1;
+
 
 
 #another way is to calculate the answer rate and output the highest one
