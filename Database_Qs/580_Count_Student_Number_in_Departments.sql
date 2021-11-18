45d6c03ebad56c8b1f9046bab361ad747ed20fa1
Claire Chiang <clairechiang91@gmail.com>
2021-11-17 22:21:34 -0500

    Update 580_Count_Student_Number_in_Departments.sql

--- Database_Qs/580_Count_Student_Number_in_Departments.sql
+++ Database_Qs/580_Count_Student_Number_in_Departments.sql
@@ -1,9 +1,7 @@
 
 # Write your MySQL query statement below
 
-SELECT dept_name, SUM(CASE WHEN stud.dept_id IS NOT NULL THEN 1 ELSE 0 END) AS student_number
-FROM department dept
-LEFT JOIN student stud
-ON dept.dept_id=stud.dept_id
-GROUP BY dept_name
-ORDER BY student_number DESC, dept_name;
+SELECT dept.dept_name, SUM(CASE WHEN stud.dept_id IS NOT NULL THEN 1 ELSE 0 END) AS student_number
+FROM Department as dept LEFT JOIN Student AS stud ON dept.dept_id=stud.dept_id
+GROUP BY dept.dept_id
+ORDER BY student_number DESC;
