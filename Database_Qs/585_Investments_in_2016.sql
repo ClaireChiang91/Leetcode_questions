70fc930bcb14f54fedbce119ddf1f6d0798c9ecf
Claire Chiang <clairechiang91@gmail.com>
2021-11-17 23:12:27 -0500

    Update 585_Investments_in_2016.sql

--- Database_Qs/585_Investments_in_2016.sql
+++ Database_Qs/585_Investments_in_2016.sql
@@ -1,20 +1,19 @@
+#From the example and test cases, the final output should be rounded. 
 
-SELECT SUM(TIV_2016) AS TIV_2016
+
+SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016 FROM Insurance 
+WHERE tiv_2015 IN (SELECT tiv_2015 FROM Insurance GROUP BY tiv_2015 HAVING COUNT(*)>1 )
+AND 
+    CONCAT(lat,lon) IN (SELECT CONCAT(lat,lon) FROM Insurance GROUP BY lat, lon HAVING COUNT(*)=1);
+
+
+
+
+
+SELECT ROUND(SUM(TIV_2016),2) AS TIV_2016
 FROM 
     insurance
     LEFT JOIN (SELECT TIV_2015, COUNT(*) AS f1 FROM insurance GROUP BY TIV_2015) a USING (TIV_2015)
     LEFT JOIN (SELECT LAT, LON, COUNT(*) AS f2 FROM insurance GROUP BY LAT, LON) b USING (LAT, LON)
 WHERE
     f1 > 1 AND f2 = 1; 
-
-
-
-
-
-
-SELECT SUM(TIV_2016) AS TIV_2016
-FROM insurance
-WHERE
-    TIV_2015 IN (SELECT TIV_2015 FROM insurance GROUP BY TIV_2015 HAVING COUNT(*) > 1)
-AND
-    CONCAT(LAT, LON) IN (SELECT CONCAT(LAT, LON) FROM insurance GROUP BY LAT, LON HAVING COUNT(*) = 1); 
